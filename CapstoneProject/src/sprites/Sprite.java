db29c511d46cbdaa051a143ba7ed36617119e9aa
Johnbok <jlee218@student.fuhsd.org>
2022-05-03 15:15:50 -0700

    Update Sprite.java

--- CapstoneProject/src/sprites/Sprite.java
+++ CapstoneProject/src/sprites/Sprite.java
@@ -1,5 +1,16 @@
 package sprites;
 
 public class Sprite {
-
+	
+	double x, y;
+	
+	public Sprite() {
+		x = 0;
+		y = 0;
+	}
+	
+	public Sprite(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
 }
