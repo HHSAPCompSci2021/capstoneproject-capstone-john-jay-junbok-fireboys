35e3b53827d2e9815de4623506745758ba96bf47
jaydalal3000 <83383435+jaydalal3000@users.noreply.github.com>
2022-05-03 15:13:04 -0700

    Screen

--- CapstoneProject/src/screen/GameScreen.java
+++ CapstoneProject/src/screen/GameScreen.java
@@ -1,5 +1,33 @@
 package screen;
 
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import main.DrawingSurface;
+import sprites.Player;
+import sprites.Sprite;
+
 public class GameScreen {
+
+	private DrawingSurface surface;
+	private Rectangle screenRect;
+	private Player player;
+	private List<Sprite> obstacles;
+	
+
+	public GameScreen(DrawingSurface surface, Level a) {
+		super(800,600);
+		this.surface = surface;
+		screenRect = new Rectangle(0,0, DRAWING_WIDTH, DRAWING_HEIGHT);
+		obstacles = new ArrayList<Sprite>();
+		obstacles.add(new Sprite(0,250,100,50));
+		obstacles.add(new Sprite(700,250,100,50));
+		obstacles.add(new Sprite(200,400,400,50));
+		obstacles.add(new Sprite(375,300,50,100));
+		obstacles.add(new Sprite(300,250,200,50));
+	}
 
 }
