869a8badcb0c7d8c856cb4c31515a84c3e619451
jaydalal3000 <83383435+jaydalal3000@users.noreply.github.com>
2022-05-06 11:08:48 -0700

    javadoc DrawingSurface.

--- CapstoneProject/src/main/DrawingSurface.java
+++ CapstoneProject/src/main/DrawingSurface.java
@@ -8,16 +8,26 @@ import screen.Screen;
 import screen.ScreenSwitcher;
 import screen.StartingScreen;
 
+/**
+ * This class represents the surface on which the screens and interactive sprites will be drawn. It retains the ability to swtich between screens since it implements ScreenSwitcher.
+ * @author jaydalal
+ * @version 05/06/2022
+ *
+ */
 public class DrawingSurface extends PApplet implements ScreenSwitcher {
 
+	/**
+	 * Represents the width and height of the drawing surfaces.
+	 */
 	public float x, y;
-	private ArrayList<Integer> keys;
 	private Screen current;
 	private ArrayList<Screen> screens;
 	
+	/**
+	 * Constructs a DrawingSurface by creating the StartingScreen, GameScreen, and EscapeScreen for this game and stroing them in an ArrayList of Screens.
+	 */
 	public DrawingSurface() {
 		screens = new ArrayList<Screen>();
-		keys = new ArrayList<Integer>();
 		
 		StartingScreen screen1 = new StartingScreen(this);
 		screens.add(screen1);
@@ -33,6 +43,9 @@ public class DrawingSurface extends PApplet implements ScreenSwitcher {
 		
 	}
 	
+	/**
+	 * Draws the DrawingSurface by illustrating the screen and needs to be currently displayed.
+	 */
 	public void draw() {
 		x = (float) width / current.DRAWING_WIDTH;
 		y = (float) height / current.DRAWING_HEIGHT;
@@ -47,6 +60,10 @@ public class DrawingSurface extends PApplet implements ScreenSwitcher {
 	}
 
 	@Override
+	/**
+	 * Switches the drawing panel to the new desired screen, represented by i
+	 * @param i the integer value that represent the desired type of screen.
+	 */
 	public void switchScreen(int i) {
 		current = screens.get(i);
 		
