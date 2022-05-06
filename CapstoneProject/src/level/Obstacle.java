b2a525a3c4332304da72057762516ca4f34b7da4
John Tahk <john.tahk@gmail.com>
2022-05-06 14:49:36 -0700

    Alpha Release

--- CapstoneProject/src/level/Obstacle.java
+++ CapstoneProject/src/level/Obstacle.java
@@ -1,5 +1,16 @@
 package level;
 
 public class Obstacle {
-
+	private double x,y;
+	private boolean solid = false;
+	
+	public Obstacle(double x, double y) {
+		this.x = x;
+		this.y = y;
+		solid = true;
+	}
+	
+	public void fade() {
+		
+	}
 }
