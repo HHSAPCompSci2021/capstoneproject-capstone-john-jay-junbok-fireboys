4659e441887996fd2db46938e27353ff04046553
jaydalal3000 <83383435+jaydalal3000@users.noreply.github.com>
2022-05-13 14:24:29 -0700

    fixed merge

--- CapstoneProject/src/level/FirstLevel.java
+++ CapstoneProject/src/level/FirstLevel.java
@@ -2,6 +2,7 @@ package level;
 
 import java.util.ArrayList;
 
+import jdalal464.shapes.Rectangle;
 import sprites.Enemy;
 import sprites.InvisCloak;
 import sprites.Player;
@@ -44,7 +45,7 @@ public class FirstLevel extends Level {
 	 *Getter method that gets the obstacles
 	 * @return super.getObstacles() an arraylist of type obstacles that locates obstacles
 	 */
-	public ArrayList<Obstacle> getObstacles() {
+	public ArrayList<Rectangle> getObstacles() {
 		return super.getObstacles();
 	}
 	/**
