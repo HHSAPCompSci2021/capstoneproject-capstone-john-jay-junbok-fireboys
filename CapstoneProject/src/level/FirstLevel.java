64d342d399d4c63c38d8b92394d8f1e7f851a747
John Tahk <john.tahk@gmail.com>
2022-05-13 14:31:36 -0700

    fixed spacing

--- CapstoneProject/src/level/FirstLevel.java
+++ CapstoneProject/src/level/FirstLevel.java
@@ -18,20 +18,13 @@ public class FirstLevel extends Level {
 	 * @param filename string with the filename.
 	 */
 	public FirstLevel () {
-		super("mazefiles/level1.txt", 600, 800);
-		addInvisCloak(new InvisCloak(130,50)); 
-		addInvisCloak(new InvisCloak(185,400));
-		addInvisCloak(new InvisCloak(300,375)); 
-		addInvisCloak(new InvisCloak(750,375));
-		addInvisCloak(new InvisCloak(750, 100));
-		addMonster(new Enemy(275,75)); 
-		addMonster(new Enemy(175,150));
-		addMonster(new Enemy(500,75)); 
-		addMonster(new Enemy(600,160));
-		addMonster(new Enemy(750,300)); 
-		addMonster(new Enemy(50,325));
-		addMonster(new Enemy (150,350)); 
-		addMonster(new Enemy(300,130));
+		super("mazefiles/level1.txt", 600, 800); addInvisCloak(new InvisCloak(130,50)); 
+		addInvisCloak(new InvisCloak(185,400)); addInvisCloak(new InvisCloak(300,375)); 
+		addInvisCloak(new InvisCloak(750,375)); addInvisCloak(new InvisCloak(750, 100));
+		addMonster(new Enemy(275,75)); addMonster(new Enemy(175,150));
+		addMonster(new Enemy(500,75)); addMonster(new Enemy(600,160));
+		addMonster(new Enemy(750,300)); addMonster(new Enemy(50,325));
+		addMonster(new Enemy (150,350)); addMonster(new Enemy(300,130));
 		
 	}
 	/**
