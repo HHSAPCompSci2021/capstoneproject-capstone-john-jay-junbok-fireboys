79108661016bc7ccafa6e789632b46c249937a8e
jaydalal3000 <83383435+jaydalal3000@users.noreply.github.com>
2022-05-22 09:23:33 -0700

    edited drawing surface

--- CapstoneProject/src/main/DrawingSurface.java
+++ CapstoneProject/src/main/DrawingSurface.java
@@ -62,9 +62,9 @@ public class DrawingSurface extends PApplet implements ScreenSwitcher, JayLayerL
 		
 		current = screens.get(0);
 	
-		p = new JayLayer("sound/", "sound/", false);
+		p = new JayLayer("sounds/", "sounds/", false);
 		p.addPlayList();
-		p.addSongs(0, new String[] {"mysterbackgroundMusic.mp3"});
+		p.addSongs(0, new String[] {"mysterybackgroundMusic.mp3"});
 		p.changePlayList(0);
 		p.addJayLayerListener(this);
 		p.nextSong();
